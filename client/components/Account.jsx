767b7f3e0fe6250472ff3d354c2896c9227c7a5f
hobartwang <hobart.cn@gmail.com>
2016-04-03 15:12:33 +0800

    三条路上的数据读写(请求api)
    meteor add http
    
    sublime 安装包 babel，然后将语法改成 babel

--- client/components/Account.jsx
+++ client/components/Account.jsx
@@ -6,7 +6,41 @@ const {
 } = mui;
 
 Account = React.createClass({
+  getInitialState() {
+    return {
+      user: {},
+    };
+  },
+  
+  _handleSubmit(e) {
+    e.preventDefault();
+    const username = this.refs.username.getValue();
+    const url = `https://api.github.com/users/${username}`;
+
+    HTTP.call('get',url,(error,res) => {
+      if(error) {
+        console.log(error);
+      } else {
+        this.setState({user: JSON.parse(res.content)})
+      }
+    });
+  },
+
   render() {
+    let GitHubInfo;
+    if(!_.isEmpty(this.state.user)) {
+      GitHubInfo = (
+        <div>
+          <UserInfo userInfo={this.state.user} />
+          <RaisedButton
+            style={{display:'block',margin:'30px auto 0',width:'180px'}}
+            primary={true}
+            label="save"
+            onClick={this._handleClick} 
+          />
+        </div>
+      );
+    }
     return (
       <div className='account'>
         <Card className='content'>
@@ -20,6 +54,7 @@ Account = React.createClass({
               primary={true}
               label="search github" />
           </form>
+          { GitHubInfo }
         </Card>
       </div>
     );
