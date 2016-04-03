fdc24cf661f9f6c2547a6b54f54ab124ca541d7b
hobartwang <hobart.cn@gmail.com>
2016-04-03 22:37:54 +0800

    响应式侧边栏

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,4 @@
-const { Link } = ReactRouter;
+const { AppBar } = mui;
 App = React.createClass({
 	mixins: [ReactMeteorData],
 	getMeteorData() {
@@ -6,13 +6,54 @@ App = React.createClass({
 			currentUser: Meteor.user()
 		}
 	},
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  componentWillMount() {
+    let setNavBarState = function(){
+      this.setState({renderNavBar: document.body.clientWidth > 700});
+    }.bind(this);
+    setNavBarState();
+    window.onresize = setNavBarState;
+  },
   render(){
     return (
       <div className="app-wrap">
-        <NavBar currentUser={this.data.currentUser}/>
+        { this.state.renderNavBar ? <NavBar currentUser={this.data.currentUser}/> :
+          this.data.currentUser ? this._getLoginAppBar() : this._getAppBar()
+        }
+        <AppLeftNav ref="leftNav" currentUser={this.data.currentUser} />
         { this.props.children }
         <Footer />
       </div>
     );
+  },
+  _getAppBar() {
+    return (
+      <AppBar title={this._getAppBarTitle()}
+        onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap}
+      />
+    );
+  },
+  _getLoginAppBar() {
+    return (
+      <AppBar title={this._getAppBarTitle()}
+        onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap}
+        iconStyleRight={{marginTop:0}}
+        iconElementRight={<LogOutMenu currentUser={this.data.currentUser} /> }
+      />
+    );
+  },
+  _getAppBarTitle() {
+    return title = this.context.router.isActive('/home') ? 'Home' :
+      this.context.router.isActive('/signup') ? 'Sign Up' :
+      this.context.router.isActive('/login') ? 'Log In' :
+      this.context.router.isActive('/account') ? 'Account' :
+      this.context.router.isActive('/chat') ? 'Chat' : '';
+  },
+  _onLeftIconButtonTouchTap() {
+    this.refs.leftNav.handleToggle();
   }
+
+
 });
